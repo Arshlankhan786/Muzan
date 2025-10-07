b3ff20c342131149e9faaf598495d54c8cbbb8bf
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2025-10-07 20:26:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -78,7 +78,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 
 # Movie Update Notification Settings/ Auto Index Settings
 
-MOVIE_UPDATE_NOTIFICATION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', True))  # Notification On (True) / Off (False)
+MOVIE_UPDATE_NOTIFICATION = bool(environ.get('MOVIE_UPDATE_NOTIFICATION', False))  # Notification On (True) / Off (False)
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', ''))  # Notification of sent to your channel
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001672766292').split()] # Auto Index Channel
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','')) # Channel to delete file from DB
